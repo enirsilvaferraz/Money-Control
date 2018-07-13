01f579464cef8aaa7ffd400586d8e8a89122d28f
Enir F Silva Ferraz <enirsilvaferraz@gmail.com>
2018-07-12 21:01:43 -0300

    Configuração inicial do espresso

--- app/src/androidTest/java/com/system/moneycontrol/ui/transactionmanager/TransactionManagerActivityTest.kt
+++ app/src/androidTest/java/com/system/moneycontrol/ui/transactionmanager/TransactionManagerActivityTest.kt
@@ -1,15 +1,14 @@
 package com.system.moneycontrol.ui.transactionmanager
 
 import android.support.test.espresso.Espresso
-import android.support.test.espresso.ViewAssertion
-import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.system.moneycontrol.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import com.system.moneycontrol.R
 
 @RunWith(AndroidJUnit4::class)
 class TransactionManagerActivityTest {
@@ -19,6 +18,23 @@ class TransactionManagerActivityTest {
 
     @Test
     fun init() {
-        Espresso.onView(ViewMatchers.withId(R.id.paymentTypeContainer)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.paymentType))
+                .check(ViewAssertions.matches(ViewMatchers.withText(R.string.system_empty_field)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.purchaseDate))
+                .check(ViewAssertions.matches(ViewMatchers.withText(R.string.system_empty_field)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.paymentDate))
+                .check(ViewAssertions.matches(ViewMatchers.withText(R.string.system_empty_field)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.price))
+                .check(ViewAssertions.matches(ViewMatchers.withText(R.string.system_empty_field)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.refund))
+                .check(ViewAssertions.matches(ViewMatchers.withText(R.string.system_empty_field)))
+
+        Espresso.onView(ViewMatchers.withId(R.id.content))
+                .check(ViewAssertions.matches(ViewMatchers.withText(R.string.system_empty_field)))
     }
 }
