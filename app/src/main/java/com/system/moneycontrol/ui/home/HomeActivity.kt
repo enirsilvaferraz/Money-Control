6318cd94ec97806cb3115d712853f652a8c2b676
Enir F Silva Ferraz <enirsilvaferraz@gmail.com>
2018-08-15 18:39:35 -0300

    Novo layout da lista de transações

--- app/src/main/java/com/system/moneycontrol/ui/home/HomeActivity.kt
+++ app/src/main/java/com/system/moneycontrol/ui/home/HomeActivity.kt
@@ -1,15 +1,10 @@
 package com.system.moneycontrol.ui.home
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AlertDialog
-import android.support.v7.app.AppCompatActivity
 import com.system.moneycontrol.R
 import com.system.moneycontrol.ui.transactionmanager.TransactionManagerActivity
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : DaggerAppCompatActivity() {
 
--- app/src/main/java/com/system/moneycontrol/ui/transactionmanager/TransactionManagerActivity.kt
+++ app/src/main/java/com/system/moneycontrol/ui/transactionmanager/TransactionManagerActivity.kt
@@ -3,7 +3,6 @@ package com.system.moneycontrol.ui.transactionmanager
 import android.os.Bundle
 import com.system.moneycontrol.R
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.android.synthetic.main.activity_transaction_manager.*
 
 
 class TransactionManagerActivity : DaggerAppCompatActivity() {
@@ -11,6 +10,6 @@ class TransactionManagerActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction_manager)
-        setSupportActionBar(toolbar)
+//        setSupportActionBar(toolbar)
     }
 }
